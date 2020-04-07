abc1fe9bc869c7e7a5cf8e5eef18245b5fa106ff
Shrey Dubey <wshrey09@gmail.com>
2020-04-07 12:34:05 +0530

    add admin routes

--- middleware/index.js
+++ middleware/index.js
@@ -12,7 +12,7 @@ middlewareObj.checkCampgroundOwnership = function(req,res,next)
                         res.redirect("back")
                     }
                     else{
-                        if(foundCampground.author.id.equals(req.user._id))    
+                        if(foundCampground.author.id.equals(req.user._id) || req.user.isAdmin)    
                             {
                                 next();
                             }
@@ -39,7 +39,7 @@ middlewareObj.checkCommentOwnership = function(req,res,next)
                 res.redirect("back")
             }
             else{
-                if(foundComment.author.id.equals(req.user._id))    
+                if(foundComment.author.id.equals(req.user._id)||req.user.isAdmin)    
                     {
                        next();
                     }
